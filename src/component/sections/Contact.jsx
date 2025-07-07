9d33da92f0c526739d664794c380d0d877c2392c
Ahmed Samir <ahmed.orgline.com>
2025-07-07 23:29:49 +0300

    MailJS

--- src/component/sections/Contact.jsx
+++ src/component/sections/Contact.jsx
@@ -1,19 +1,40 @@
+import { useRef } from "react";
 import { Title } from "./title";
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_6g24gt3', 'template_imn9jer', form.current, {
+            publicKey: 'WNVR_tWPumhREht6F',
+        })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
     return (
         <>
             <Title title="Contact US" />
             <div className="container mx-auto max-w-4xl text-center">
-                <form className="mt-12 grid gap-6 max-w-xl mx-auto" data-aos="fade-up"
+                <form ref={form} onSubmit={sendEmail} className="mt-12 grid gap-6 max-w-xl mx-auto" data-aos="fade-up"
                     data-aos-anchor-placement="top-center">
                     <input
                         type="text"
+                        name="user_name"
                         placeholder="Your Name"
                         className="w-full border text-white placeholder:text-gray-300 border-gray-300 rounded px-4 py-3 focus:outline-none focus:border-green-600"
                     />
                     <input
                         type="email"
+                        name="user_email"
                         placeholder="Your Email"
                         className="w-full border text-white placeholder:text-gray-300 border-gray-300 rounded px-4 py-3 focus:outline-none focus:border-green-600"
                     />
@@ -21,6 +42,7 @@ export const Contact = () => {
                         placeholder="Your Message"
                         className="w-full text-white placeholder:text-gray-300 border border-gray-300 rounded px-4 py-3 focus:outline-none focus:border-green-600"
                         rows="5"
+                        name="message"
                     ></textarea>
                     <button
                         type="submit"
@@ -29,7 +51,7 @@ export const Contact = () => {
                         Send Message
                     </button>
                 </form>
-                
+
             </div>
         </>
     );
