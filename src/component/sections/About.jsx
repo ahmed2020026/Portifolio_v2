90613fc670524d6a83a800eee37bc63929b83d50
Ahmed Samir <ahmed.orgline.com>
2025-07-21 17:46:46 +0300

    addCv

--- src/component/sections/About.jsx
+++ src/component/sections/About.jsx
@@ -12,7 +12,7 @@ export const About = () => {
                     <div className="About-info col-span-2" data-aos="fade-up" data-aos-duration="1500">
                         <h1 className="font-bold text-3xl text-white mb-5">Front-End Developer</h1>
                         <p className="text-gray-400">Frond-End Developer with a strong passion for transforming creative ideas into engaging and seamless web user interface.I have established a solid foundation in JavaScript & modern framework like React, along with libraries such as jQuery & mastered HTML5, CSS3 and CSS frameworks like tailwind CSS and Bootstrap.l believe in the importance of continuous learning and always seeking to explore the latest technologies and best practices in web development</p>
-                        <a href="#" className="px-3 py-2 bg-green-600 text-white mt-5 rounded inline-block hover:bg-green-800 transition-all">DownLoad CV</a>
+                        <a href="./ASM_CV.pdf" download className="px-3 py-2 bg-green-600 text-white mt-5 rounded inline-block hover:bg-green-800 transition-all">DownLoad CV</a>
                     </div>
                 </div>
             </div>
