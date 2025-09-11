cb1a5345ad9a60d678f537f31173bd85a3c4847b
Ahmed Samir <ahmed.orgline.com>
2025-09-11 14:39:31 +0300

    update_cv

--- src/component/sections/About.jsx
+++ src/component/sections/About.jsx
@@ -40,7 +40,7 @@ export const About = () => {
                         </p>
 
                         <a
-                            href="./Ahmed_Samir_FrontEnd_CV"
+                            href="./Ahmed_Samir_FrontEnd_CV.pdf"
                             download
                             className="px-5 py-3 bg-green-600 text-white mt-6 rounded-lg inline-flex items-center gap-2 hover:bg-green-700 transition-all shadow-md hover:shadow-green-600/40"
                         >
