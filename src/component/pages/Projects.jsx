8809bddf7c0d265996dc3080c6c497d2267a3869
Ahmed Samir <ahmed.orgline.com>
2025-09-11 14:48:38 +0300

    ani

--- src/component/pages/Projects.jsx
+++ src/component/pages/Projects.jsx
@@ -3,7 +3,7 @@ import { Projects } from "../sections/Projects";
 const ProjectsPage = () => {
     return (
         <>
-            <section id="Projects" className="pt-30 min-h-dvh pb-10 md:pb-0">
+            <section id="Projects" className="pt-30 min-h-dvh md:pb-0">
                 <Projects />
             </section>
         </>
--- src/component/sections/Projects.jsx
+++ src/component/sections/Projects.jsx
@@ -11,9 +11,8 @@ export const Projects = () => {
                 <div className="Project-content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                     {
                         ProjectsData.map((project, index) => {
-                            //console.log(project.address.url)
                             return (
-                                <div key={index} className="group overflow-hidden relative" data-aos="zoom-in" data-aos-duration='1500' data-aos-delay={`${Number(index + 1) * 200}`}>
+                                <div key={index} className="group overflow-hidden relative" data-aos="fade-zoom-in" data-aos-duration='1500' data-aos-delay={`${Number(index*100)}`}>
                                     <img src={project.img} className="w-full transition-all duration-500 h-full" />
                                     <div className="source absolute top-0 left-0 w-full h-full flex items-center justify-center bg-[#21222236] opacity-100 transition-opacity duration-500">
                                         <a href={`${project.address.url}`} target='_blank' rel='noopener noreferrer' className="text-white p-2 bg-green-600 mx-2 rounded">
